11ca8babbcb0a11cacda9add5bc66d6957a44abb
Sol <alvaro.figueroa3206@gmail.com>
2026-01-11 18:03:17 +0100

    En dos han sido

--- src/app/pages/directivas/directivas.ts
+++ src/app/pages/directivas/directivas.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-directivas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './directivas.html',
   styleUrl: './directivas.css',
 })
-export class Directivas {
 
+export class Directivas {
+  activo:boolean = false;
+  color:string = "rojo";
+  listaClientes:any[] = [
+    {
+      nombre:"Hans Burger",
+      puesto:"10x Developer",
+      cantidad:863.45,
+      estado:"Aprobado",
+      fecha:"06/10/2020"
+    },
+    {
+      nombre:"Sofia ramirez",
+      puesto:"UI Designer",
+      cantidad:540,
+      estado:"Pendiente",
+      fecha:"07/15/2020"
+    },
+    {
+      nombre:"Liam Wong",
+      puesto:"Project Manager",
+      cantidad:1200.99,
+      estado:"Aprobado",
+      fecha:"08/01/2020"
+    },
+    {
+      nombre:"Emma Johnson",
+      puesto:"QA Engineer",
+      cantidad:320.5,
+      estado:"Pendiente",
+      fecha:"08/20/2020"
+    },
+    {
+      nombre:"Carlos Vega",
+      puesto:"Backend Developer",
+      cantidad:980.75,
+      estado:"Aprobado",
+      fecha:"09/05/2020"
+    }
+  ]
 }
--- src/app/pages/inicio/inicio.ts
+++ src/app/pages/inicio/inicio.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
-export class Inicio {
 
+export class Inicio {
+  interpolacion:string = "Interpolacion";
+  logo =  '/assets/logo-litterator.jpeg';
+  nombre:string = ''; 
+  mostrarMensaje(){
+    alert('El boton funciona')
+  }
 }
