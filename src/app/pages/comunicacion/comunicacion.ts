6cb829c8b4dcf9c2a139330006d451a516305fc1
Sol <alvaro.figueroa3206@gmail.com>
2026-01-15 13:03:37 +0100

    Mas cosillas de padre e hijo

--- src/app/pages/comunicacion/comunicacion.ts
+++ src/app/pages/comunicacion/comunicacion.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { Padre } from './padre/padre';
 import { Hijo } from './hijo/hijo';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-comunicacion',
-  imports: [Hijo, CommonModule, Padre],
+  imports: [Hijo, CommonModule, FormsModule],
   templateUrl: './comunicacion.html',
   styleUrl: './comunicacion.css',
 })
 export class Comunicacion {
+  showModal = false;
 
+  mensajeDelPadre:string = '';
+  mensajeDelHijo:string = '';
+
+  openModal(){
+    console.log('Modal abierto');
+    this.showModal = true;
+  }
+
+  closeModal(){
+    console.log('Modal cerrado');
+    this.showModal = false;
+  }
+
+  recibirMensajeDelHijo(event:any){
+    this.mensajeDelHijo = event;
+  }
 }
--- src/app/pages/comunicacion/hijo/hijo.ts
+++ src/app/pages/comunicacion/hijo/hijo.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hijo',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './hijo.html',
   styleUrl: './hijo.css',
 })
 export class Hijo {
+  @Input() mensajeDelPadre: string | null = null;
 
+  @Output() eventoDelHijo = new EventEmitter<string>();
+
+  mensaje:string = '';
+
+  enviarMensajeDelHijo(){
+    this.eventoDelHijo.emit(this.mensaje)
+  }
 }
